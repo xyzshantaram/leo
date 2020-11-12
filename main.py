48934c85d725c0837274c81c8f4f08b063b1c886
Siddharth S Singh <simsid66@gmail.com>
2020-11-13 01:38:45 +0530

    url validation, state, redirect handling

--- main.py
+++ main.py
@@ -1,48 +1,108 @@
 import socket
 import ssl
 import sys
+import curses
+import re
+
+state = {
+    "current_hname": "",
+    "current_url": "",
+    "sock": None,
+    "ssock": None,
+    "context": ssl.create_default_context(),
+    "logger": print,
+    "render_body": []
+}
+
+state["context"].check_hostname = False
+state["context"].verify_mode = ssl.CERT_NONE
+
 
 def validate_url(url):
-    if ("gemini://" not in url):
-        return "gemini://" + url
+    if (re.match(r"((gemini:\/\/)?[a-zA-Z0-9\-]+\.[a-zA-Z0-9])?\/?[^\/\t\n\ \r]+(\/?[^\/\/\n\ \r\t]+)*\/?", url)):
+        if ("gemini://" not in url):
+            if (re.match(r"[a-zA-Z0-9\-]+\.[a-zA-Z].*", url)):
+                return "gemini://" + url + "/"
+            else:
+                if (url[0].isalnum()):
+                    url = state["current_url"] + url
+                    if not url.endswith("/"):
+                        url += "/"
+                    return url
+        else:
+            return url
     else:
-        return url
+        if (url == "/"):
+            return validate_url(state["current_hname"])
+        return None
+
+
+def get_hostname(url):
+    ret = re.findall(
+        r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]", url)[0]
+    if not (ret.endswith("/")):
+        return ret
+    else:
+        return ret[:len(ret) - 1]
+
 
 def get_encoded(s):
     return ((s + "\r\n").encode("UTF-8"))
 
-if len(sys.argv) == 1:
-    hostname, port = input("Enter hostname and port (colon-separated): ").split(":")
-    port = int(port)
-else:
-    hostname, port = sys.argv[1].split(":")
-    port = int(port)
 
-context = ssl.create_default_context()
-context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
+def log(*argv):
+    if (state["logger"]):
+        state["logger"](*argv)
 
-def gemini_get_document(url, port, logger):
-    sock = socket.create_connection((hostname, port))
-    ssock = context.wrap_socket(sock, server_hostname=hostname)
-    if logger:
-        logger("Connected to", hostname, "over", ssock.version())
-    ssock.sendall(get_encoded(validate_url(hostname)))
-    fp = ssock.makefile("rb")
+
+def gemini_get_document(url, port):
+    url = validate_url(url)
+    state["current_url"] = url
+    log("attempting to get", url)
+    hostname = get_hostname(url)
+    if state["current_hname"] != hostname:
+        state["sock"] = socket.create_connection((hostname, port))
+        state["ssock"] = state["context"].wrap_socket(
+            state["sock"], server_hostname=hostname)
+        log("Connected to", hostname, "over", state["ssock"].version())
+    else:
+        pass
+    state["current_hname"] = hostname
+    state["ssock"].sendall(get_encoded(url))
+    fp = state["ssock"].makefile("rb")
     header = fp.readline()
-    header = header.decode("UTF-8").strip()
-    status, mime = header.split()
-    response_body = fp.read().decode("UTF-8").strip().split("\n")
-    
+    header = header.decode("UTF-8").strip().split()
+    status = header[0]
+    meta = header[1]
+    response_body = None
+    if (status.startswith("2")):
+        response_body = fp.read().decode("UTF-8").strip().split("\n")
+        fp.close()
+    elif (status.startswith("3")):
+        fp.close()
+        log("redirected to", meta)
+        return gemini_get_document(meta, 1965)
+
     return {
         "status": status,
-        "mime": mime,
+        "meta": meta,
         "body": response_body
     }
 
-a = gemini_get_document(hostname, port, print)
-print(hostname, "returned code", a["status"])
-print("MIME type of body was", a["mime"]) if a["status"] == "20" else ""
+
+if len(sys.argv) == 1:
+    url = input("Enter hostname / URL: ")
+else:
+    url = sys.argv[1]
+
+a = gemini_get_document(url, 1965)
+
+print(get_hostname(url), "returned code", a["status"])
+print(validate_url("users/"))
+print("MIME type of body was", a["meta"]) if a["status"] == "20" else ""
 if (a["body"]):
     for i in a["body"]:
-        print(i.replace("\\n", "\n"))
+        line = i
+        state["render_body"].append(i.replace("\\n", "\n"))
+    for i in state["render_body"]:
+        print(i)
