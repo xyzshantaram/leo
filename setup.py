d8398f00312eb02f3571040696b296289d139240
Siddharth Singh <simsid66@gmail.com>
2021-05-29 22:27:30 +0530

    Begin setting up installer

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gmi-leo',
+    version='1.0.1',
+    scripts=['leo'],
+    author='shantaram',
+    author_email='me@shantaram.xyz',
+    license='MIT',
+    description='A terminal-based client for the Gemini protocol.',
+    long_description=long_description,
+    url="https://github.com/shantaram3013/leo",
+    project_urls={
+        "Bug Tracker": "https://github.com/shantaram3013/leo",
+        "README": "https://github.com/shantaram3013/leo#readme"
+    },
+    data_files=[('leo', ['config.json'])],
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta"
+    ],
+    keywords='leo gemini gopher internet protocol www client',
+)
