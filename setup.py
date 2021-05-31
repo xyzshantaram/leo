88cc10a87afe2ec86be06e6ea2bcd099f5360b74
Siddharth Singh <simsid66@gmail.com>
2021-05-31 19:52:30 +0530

    Update version to 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='leo-gmi',
-    version='1.0.3',
+    version='1.0.4',
     scripts=['leo'],
     author='shantaram',
     author_email='me@shantaram.xyz',
