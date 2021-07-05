669bd4e0f548025a8d55c2fc8e0bf0d9ef4e343b
Siddharth Singh <simsid66@gmail.com>
2021-07-05 19:36:33 +0530

    Allow issuing commands during paging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='leo-gmi',
-    version='1.0.5',
+    version='1.0.6',
     scripts=['leo'],
     author='shantaram',
     author_email='me@shantaram.xyz',
