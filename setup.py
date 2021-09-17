2adc2da38fa515f52fffccc5049dba7ab100e305
Siddharth Singh <simsid66@gmail.com>
2021-09-17 11:45:38 +0530

    Highlight empty lines.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='leo-gmi',
-    version='1.2.0',
+    version='1.2.1',
     scripts=['leo'],
     author='shantaram',
     author_email='me@shantaram.xyz',
